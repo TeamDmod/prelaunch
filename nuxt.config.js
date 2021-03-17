c55b34221217a21c93c0f733bf877d765e810afd
Maxim King <73140396+MaximKing1@users.noreply.github.com>
2021-03-17 20:02:57 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,8 @@ export default {
         meta: [
             { charset: "utf-8" },
             { name: "viewport", content: "width=device-width, initial-scale=1" },
-            { hid: "description", name: "description", content: "Finding moderators or a server to work for has never been quicker! Create or apply for Applications. Even search by Rating, Time Zone, and more." }
-            { property: 'og:title', content: 'dmod.gg', vmid: 'og:title' }
+            { hid: "description", name: "description", content: "Finding moderators or a server to work for has never been quicker! Create or apply for Applications. Even search by Rating, Time Zone, and more." },
+            { property: 'og:title', content: 'dmod.gg', vmid: 'og:title' },
             { property: 'og:description', content: 'Finding moderators or a server to work for has never been quicker! Create or apply for Applications. Even search by Rating, Time Zone, and more.', vmid: 'og:description' }
         ],
         link: [{ rel: "icon", type: "image/x-icon", href: "/logo.png" }]
