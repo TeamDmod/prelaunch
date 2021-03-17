ef1a9cf319fcf81a1d885d449c54a24cea74f74a
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-03-17 13:58:15 -0600

    Update METAs

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,9 @@ export default {
         meta: [
             { charset: "utf-8" },
             { name: "viewport", content: "width=device-width, initial-scale=1" },
-            { hid: "description", name: "description", content: "" }
+            { hid: "description", name: "description", content: "Finding moderators or a server to work for has never been quicker! Create or apply for Applications. Even search by Rating, Time Zone, and more." }
+            { property: 'og:title', content: 'dmod.gg', vmid: 'og:title' }
+            { property: 'og:description', content: 'Finding moderators or a server to work for has never been quicker! Create or apply for Applications. Even search by Rating, Time Zone, and more.', vmid: 'og:description' }
         ],
         link: [{ rel: "icon", type: "image/x-icon", href: "/logo.png" }]
     },
