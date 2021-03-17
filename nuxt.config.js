27caf45de60424fb373992736eb72ff8423f143c
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-03-17 14:20:36 -0600

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,8 @@ export default {
             { name: "viewport", content: "width=device-width, initial-scale=1" },
             { hid: "description", name: "description", content: "Finding moderators or a server to work for has never been quicker! Create or apply for Applications. Even search by Rating, Time Zone, and more." },
             { property: 'og:title', content: 'dmod.gg', vmid: 'og:title' },
-            { property: 'og:description', content: 'Finding moderators or a server to work for has never been quicker! Create or apply for Applications. Even search by Rating, Time Zone, and more.', vmid: 'og:description' }
+            { property: 'og:description', content: 'Finding moderators or a server to work for has never been quicker! Create or apply for Applications. Even search by Rating, Time Zone, and more.', vmid: 'og:description' },
+            { property: 'og:image', content: "/logo.png" || '' },
         ],
         link: [{ rel: "icon", type: "image/x-icon", href: "/logo.png" }]
     },
