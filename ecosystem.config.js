08d04c1c70d2d4c979cd1fe89dd087be41c2a12e
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-05-29 18:27:37 +0100

    Removed old files

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps: [{
-        name: 'dmod.gg',
-        exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start'
-    }]
-}
