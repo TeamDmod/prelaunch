27ef8add8b3464f6e9c2dbe198a86c93057c0d31
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-07-30 16:31:45 +0100

    🎯 Server-Side Redirect

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -1,16 +1,21 @@
-import Head from 'next/head';
 import MetaTags from 'components/MetaTags';
+
+export async function getStaticProps() {
+	return {
+		redirect: {
+			permanent: true,
+			destination: 'https://discord.com/invite/429zwpugYf',
+		},
+	};
+}
 
 export default function Discord() {
 	return (
 		<>
-			<MetaTags title='Dmod Discord' />
-			<Head>
-				<meta
-					httpEquiv='refresh'
-					content='0;url=https://discord.com/invite/429zwpugYf'
-				/>
-			</Head>
+			<MetaTags
+				title='Dmod Discord'
+				description='The official dmod.gg Discord server! Join us to learn how we are innovating the Moderation Community.'
+			/>
 		</>
 	);
 }
