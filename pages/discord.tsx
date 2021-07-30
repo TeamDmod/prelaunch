700dce415cd52bb4abb97e9bb844cad35ac080e5
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-07-30 17:16:51 +0100

    📲 Fixed Redirect Meta

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -1,21 +1,25 @@
 import MetaTags from 'components/MetaTags';
-
-export async function getServerSideProps() {
-	return {
-		redirect: {
-			permanent: true,
-			destination: 'https://discord.com/invite/429zwpugYf',
-		},
-	};
-}
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Discord() {
+	const router = useRouter();
+
+	useEffect(() => {
+		router.replace('https://discord.com/invite/429zwpugYf');
+	}, []);
+
 	return (
 		<>
 			<MetaTags
 				title='Dmod Discord'
 				description='The official dmod.gg Discord server! Join us to learn how we are innovating the Moderation Community.'
 			/>
+			<main>
+				<h1>
+					<small>Redirecting...</small>
+				</h1>
+			</main>
 		</>
 	);
 }
