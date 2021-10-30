58d66e3e6bf579565951d592fe1fbc12f60cce7e
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-10-30 17:27:39 +0100

    🔗 Link Fixes

--- components/MetaTags.tsx
+++ components/MetaTags.tsx
@@ -15,7 +15,7 @@ export default function Metatags({
 			<meta name='theme-color' content={color} />
 
 			<meta name='twitter:card' content='summary' />
-			<meta name='twitter:site' content='@dmod.gg' />
+			<meta name='twitter:site' content='@TeamDmod' />
 			<meta name='twitter:title' content={title} />
 			<meta name='twitter:description' content={description} />
 			<meta name='twitter:image' content={image} />
