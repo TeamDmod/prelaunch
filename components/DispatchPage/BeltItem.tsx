23b2089e3012bde2c51c085a02886ba09b9310c5
Daniel Banks <danielbankscoding@gmail.com>
2021-11-28 11:59:42 +0000

    fixing CSS issue

--- components/DispatchPage/BeltItem.tsx
+++ components/DispatchPage/BeltItem.tsx
@@ -1,9 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from "react";
-
-import styles from "../../styles/dispatch.module.scss";
-
 export default function BeltItem({ name, icon, invite, memCount, team }: { name:string, icon?:StaticImageData, invite:string, memCount?:string, team?:boolean  }) {
     const router = useRouter();
     
@@ -14,11 +11,93 @@ export default function BeltItem({ name, icon, invite, memCount, team }: { name:
 
     return (
 		<>
-            <div className={team ? styles.ourteamItem : styles.clienteleItem} onClick={(e) => {
+            <style jsx>{`
+                .ourteamItem {
+                    display: flex;
+                    background-color: rgba(255, 255, 255, 0.494);
+                    border-radius: 10px;
+
+                    padding: 10px 50px 10px 10px;
+                    transition: all 0.3s ease-in-out;
+
+                    animation: move 40s infinite ease-in-out;
+                    position: relative;
+                }
+
+                .ourteamItem h3 {
+                    font-weight: 400;
+                    color: #fff;
+                }
+
+                .ourteamItem h2 {
+                    font-weight: 500;
+                    color: #111;
+                    margin: 10px 0 0 10px;
+                }
+
+                .ourteamItem img {
+                    width: 50px;
+                    height: 50px;
+                    border-radius: 50%;
+                }
+
+                .ourteamItem:hover {
+                    background-color: rgba(255, 255, 255, 0.7);
+                    cursor: pointer;
+                }
+
+                .staffbeltItem {
+                    display: flex;
+                    background-color: rgba(255, 255, 255, 0.494);
+                    border-radius: 10px;
+
+                    padding: 10px 50px 10px 10px;
+                    transition: all 0.3s ease-in-out;
+
+                    animation: move 40s infinite ease-in-out;
+                    position: relative;
+                }
+
+                .staffbeltItem img {
+                    width: 50px;
+                    height: 50px;
+                    border-radius: 50%;
+                }
+
+                .staffbeltItem h2 {
+                    font-weight: 500;
+                    color: #111;
+                    margin: 10px 0 0 10px;
+                }
+
+                .staffbeltItem h3 {
+                    font-weight: 400;
+                    color: #fff;
+                }
+
+                .staffbeltItem:hover {
+                    background-color: rgba(255, 255, 255, 0.7);
+                    cursor: pointer;
+                }
+
+                @keyframes move {
+                    0% {
+                        left: -50%;
+                    }
+                    50% {
+                        left: 60%;
+                    }
+                    100% {
+                        left: -50%;
+                    }
+                }
+            `}</style>
+
+            <div onClick={(e) => {
                 toInvite(e);   
-            }} key={name + " display"}>
+            }} key={name + " display"} className={team? "ourteamItem" : "staffbeltItem"}>
                 <img src={icon?.src} alt={"Dispatch Logo Image"} />
-                <h2>{name}</h2>
+                <h2 style={{ textAlign: "center", fontSize: "1.5em" }}>{name}</h2>
             </div>
             
         </>
--- pages/dispatch.tsx
+++ pages/dispatch.tsx
@@ -1,4 +1,3 @@
-import styles from '../styles/dispatch.module.scss';
 import { DispatchLogo, SquidGameLogo, WendysLogo } from 'utils/Images';
 import { useState } from 'react';
 import BeltItem from 'components/DispatchPage/BeltItem';
@@ -7,7 +6,7 @@ import { dslLogo, BughaLogo, FallGuysLogo, NZXTLogo, DankMemerLogo, DaniLogo, G2
 
 export default function Dispatch() {
 
-    let clients = [
+    const clients = [
         {
             name: 'Rize',
             url: 'discord.gg/gh7rTyEfWm',
@@ -52,7 +51,7 @@ export default function Dispatch() {
         },
     ];
 
-    let ourTeam = [
+    const ourTeam = [
         {
             name: 'Fall Guys',
             url: 'https://discord.gg/fallguys',
@@ -66,7 +65,7 @@ export default function Dispatch() {
             logo: G2Logo
         },
         {
-            name: 'Dank Memer Community',
+            name: 'Dank Memer',
             url: 'https://discord.gg/memers',
             members: "1000",
             logo: DankMemerLogo
@@ -94,27 +93,87 @@ export default function Dispatch() {
 
 
     return (
-        <div className={styles.container}>
-            <div className={styles.mainHeader}>
-              <h1 className={styles.dispatchHeader}>Dispatch Program</h1>
+        <>
+        <style jsx>{`
+            .faq {
+                width: 100%;
+                max-width: 800px;
+                margin: 0 auto;
+                padding: 0 15px;
+            }
+            
+            .question {
+                background-color: #283042;
+                border-radius: 9px;
+                margin-bottom: 10px;
+                padding: 10px;
+                box-shadow: 5px 2px 5px rgba(0, 0, 0, 0.1);
+            }
+            
+            .questionlink {
+                font-size: 1.6rem;
+                color: rgba(255, 255, 255, 0.8);
+                text-decoration: none;
+                background-color: #283042;
+                width: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                padding: 10px 0;
+            }
+            
+            .answer {
+                max-height: 0;
+                overflow: hidden;
+                position: relative;
+                background-color: #212838;
+                transition: max-height 650ms ease-in-out;
+            }
+            
+            .answer::before {
+                content: "";
+                position: absolute;
+                width: 6px;
+                height: 90%;
+                background-color: #091831;
+                top: 50%;
+                left: 0;
+                transform: translateY(-50%);
+            }
+            
+            .answer p {
+                font-size: 14px;
+                color:rgba(255, 255, 255, 0.6);
+                padding: 20px;
+            }
+            
+            .question:target .answer {
+                max-height: 200px;
+            }
+
+
+        `}</style>
+        <div style={{ background: "linear-gradient(180deg, #061021 0%, #0b2041 100%)", minHeight: "min-height: calc(100vh - 6rem)", display: "grid" }}>
+            <div style={{ textAlign: "center" }}>
+              <h1 style={{ fontWeight: 500, color: "#fff", marginTop: "25px" }}>Dispatch Program</h1>
             </div>
             <div className='top'>
-                <h1 className={styles.clienteleHeader}style={{ opacity: "0.75", marginTop: "60px" }}>Clientele</h1>
-                <p className={styles.clienteleDescription} style={{ marginBottom: "35px" }}>Whether your community is focused on competitive gaming, Web3, or a university, we'll cover your needs with reliability and care.</p>  
+                <h1 style={{ opacity: "0.75", marginTop: "60px", color: "#fff", fontSize: "2.6em", textAlign: "center", fontWeight: 400, position: "relative" }}>Clientele</h1>
+                <p style={{ marginBottom: "35px", fontSize: "1.7em" }}>Whether your community is focused on competitive gaming, Web3, or a university, we'll cover your needs with reliability and care.</p>  
             </div>
             
-            <div className={styles.clientele}>
+            <div style={{ display: "flex", flexFlow: "row wrap", justifyContent: "center", alignItems: "stretch", alignContent: "center", gap: "20px", overflow: "hidden", margin: "0px 18vw 0 18vw" }}>
                 {clients.map((client, index:number) => (
                     <BeltItem name={client.name} icon={client.logo} invite={client.url} team={false} />
                 ))}
             </div>
             
             <div className="top">
-                <h1 className={styles.clienteleHeader}style={{ opacity: "0.75", marginTop: "40px" }}>Our Team</h1>
-                <p className={styles.clienteleDescription}>Below are some of the communities our staff members have experience working with.</p>
+                <h1 style={{ opacity: "0.75", marginTop: "40px", color: "#fff", fontSize: "2.6em", textAlign: "center", fontWeight: 400, position: "relative" }}>Our Team</h1>
+                <p style={{ fontSize: "1.7em" }}>Below are some of the communities our staff members have experience working with.</p>
             </div>
             
-            <div className={styles.ourteam}>
+            <div style={{ display: "flex", flexFlow: "row wrap", justifyContent: "center", alignItems: "stretch", alignContent: "center", gap: "20px", overflow: "hidden", margin: "0px 18vw 0 18vw" }}>
                 {ourTeam.map((client, index:number) => (
                     <BeltItem name={client.name} icon={client.logo} invite={client.url} team={true}/>
                 ))}
@@ -124,45 +183,45 @@ export default function Dispatch() {
             {/* FAQ */}
             <h1 style={{ opacity: "0.75", marginBottom: "40px"}}>FAQ</h1>
 
-            <div className={styles.faq}>
-                <div className={styles.question} id="question1">
-                    <a className={styles.questionlink} href="#question1">
+            <div className="faq">
+                <div className="question" id="question1">
+                    <a className="questionlink" href="#question1">
                         How do I request a Dispatch?
                     </a>
-                    <div className={styles.answer}>
+                    <div className="answer">
                         <p>
                              If you're a community owner looking to request a dispatch, please visit our Discord community <a href="https://discord.gg/moderate" style={{color: "lightblue"}}>https://discord.gg/moderate</a> & open up a ticket in our support channel. We'll get back to you ASAP! 
                         </p>  
                     </div>
                 </div>
 
-                <div className={styles.question} id="question2">
-                    <a className={styles.questionlink} href="#question2">
+                <div className="question" id="question2">
+                    <a className="questionlink" href="#question2">
                         How do I apply to become a Dispatch moderator? 
                     </a>
-                    <div className={styles.answer}>
+                    <div className="answer">
                         <p>
                              If you would like to become a Dispatch moderator, please apply <a href="https://docs.google.com/forms/d/e/1FAIpQLSfPVHja-oy-ausc4UcPFA3jE-apA0FDEy4hm-cklKFSnVlkQw/viewform" style={{color: "lightblue"}}>here</a>. We'll review your application, and if you pass, we can move onto the interview process.
                         </p> 
                     </div>
                 </div>
                 
-                <div className={styles.question} id="question3">
-                    <a className={styles.questionlink} href="#question3">
+                <div className="question" id="question3">
+                    <a className="questionlink" href="#question3">
                         What are your community acceptance guidelines? 
                     </a>
-                    <div className={styles.answer}>
+                    <div className="answer">
                         <p>
                              In order to maintain the level of high-quality work that we want to achieve, we accept Dispatch requests based off of our own rules & regulations as well as Discord's. <a style={{color: "lightblue"}} href="https://dmod.super.site/dispatch/community-acceptance-guidelines">Our full acceptance guidelines can be found here</a>
                         </p>
                     </div>
                 </div>
 
-                <div className={styles.question} id="question4">
-                    <a className={styles.questionlink} href="#question4">
+                <div className="question" id="question4">
+                    <a className="questionlink" href="#question4">
                         Does Dmod ever dispatch moderators for free? 
                     </a>
-                    <div className={styles.answer}>
+                    <div className="answer">
                         <p>
                             We do! While we tend to work under paid contracts, we are open to dispatching to communities that can’t afford to compensate us financially.
                         </p>  
@@ -171,5 +230,6 @@ export default function Dispatch() {
             </div>
             <div style={{marginTop: "400px"}}/> 
         </div>
+        </>
     )
 }
